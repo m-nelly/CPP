5f2100cdb0ccd74832e74b4a995ea1b93ce88492
netmn <netmn99@gmail.com>
2020-08-22 14:20:10 -0500

    Added Variable Types Lesson

--- notes.cpp
+++ notes.cpp
@@ -2,6 +2,7 @@
 
 //Imports IO stream library.
 #include <iostream>
+#include <string>
 
 using namespace std;
 //A namespace is a collection of commands.
@@ -21,9 +22,10 @@ int main() //Declaration
     //This is the body of the function.
     basics_lesson();
     variables_lesson();
+    variable_types_lesson();
 }
 
-int basics_leson()
+int basics_lesson()
 {
     cout << "This is a test." << endl;
     //Sends "This is a test." to the console output and ends the line.
@@ -47,10 +49,39 @@ int variables_lesson()
 
 int variable_types_lesson()
 {
-  /*
-  Variable Types:
-  1.Integer
-  2.
-  3.
-  */  
+    //Define variable type, name, and then value
+    
+    //Multiple variables can be defined under the same type, separate with commas
+    
+    /*
+    Variable Types:
+    1.Integer (int) Number w/o decimal. Gets 4 bytes of memory.
+    2.Short Integer (short) Small number w/o decimal. Gets 2 bytes of memory.
+    3.Floating Point Number (float) Small number w/ decimal. Gets 4 bytes of memory.
+    4.Double (double) Large number w/ decimal. Gets 8 bytes of memory.
+    5.Character (char) Single Character
+    6.String (string) Multiple Characters
+    7.Boolean (bool) True/False, 1/0
+    */  
+    int a = 20;
+    short b = 100;
+    float c = 5.2;
+    double d = 420.69;
+    char e = 'a';
+    string f = "HelloWorld!";
+    bool g = true;
+    //Unsigned variables hold only positive values.
+    unsigned short h = 60000;
+    //Variables can be locked with 'const'.
+    const string permanent = "permanent";
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    cout << f << endl;
+    cout << g << endl;
+    cout << h << endl;
+    cout << permanent << endl;
 };
